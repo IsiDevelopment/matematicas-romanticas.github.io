4a9f265b2b8a7b0edf85de51f1fd686ffe610def
isidro <isidro.cano.mtz@outlook.es>
2019-01-07 01:54:57 -0600

    comimit

---
+++ JavaScript/slide_left_and_right.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#ik').addClass('c')
+    $('#ik').removeClass('d')
+}
+);
+function right_slide(){
+$(window).scroll(function(){
+    $('#ik').addClass('d')
+    $('#ik').removeClass('c')
+})
+}
+function left_slide(){
+$(window).scroll(function(){
+    $('#ik').addClass('c')
+    $('#ik').removeClass('d')
+})
+}
+$(document).scroll(function() {
+  if ( $(document).scrollTop() > 150) {
+  	right_slide()
+  } else {
+  	left_slide()
+  }
+});
+function redirect() {
+  location.replace("https://www.w3schools.com")
+}
